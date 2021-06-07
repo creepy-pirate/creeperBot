4f5e6b45ab05d8fc27d7dee18ce341e7772c3cf3
Deepak Singh <dv72276@gmail.com>
2021-06-07 13:45:13 +0530

    Created the bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:          'qQwMwSq2tl26jGUeS3bHhBbjv',
+    consumer_secret:      '6Mo1V8Ob92S7fYWmffkTfihLZgHc3toSHrxnUM06jKKLeZluFo',
+    access_token:         '1401766359780888579-Gi0VFWVmsuQf3kTvnkm4OrjWFq1jQd',
+    access_token_secret:  'JqsZj8KByhplIoYvhpUi2ldu2xLRPExNKG8q2dpjYQqzq'
+  }
