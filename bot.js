ffbc941feab53485de9077569fa909b2c8db7b0e
Deepak Singh <dv72276@gmail.com>
2021-06-07 14:54:31 +0530

    fix

--- bot.js
+++ bot.js
@@ -32,11 +32,7 @@ const retweetLatest = () => {
 };
 
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+
 
 // Try to retweet something as soon as we run the program...
 retweetLatest();
