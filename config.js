de2a53e26a61d1bce3b1ba64fbe5e8ade9a162a4
Deepak Singh <61933667+creepy-pirate@users.noreply.github.com>
2021-06-08 16:42:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:          'qQwMwSq2tl26jGUeS3bHhBbjv',
-    consumer_secret:      '6Mo1V8Ob92S7fYWmffkTfihLZgHc3toSHrxnUM06jKKLeZluFo',
+    consumer_key:          'fTcBzWUJvdkRLOEV75qiDKnjc',
+    consumer_secret:      '2HJteW5wBpgG8yeZWvOpLEVXTmEJjni9GB2NUYcdepwKoMLJsi',
     access_token:         '1401766359780888579-Gi0VFWVmsuQf3kTvnkm4OrjWFq1jQd',
     access_token_secret:  'JqsZj8KByhplIoYvhpUi2ldu2xLRPExNKG8q2dpjYQqzq'
   }
