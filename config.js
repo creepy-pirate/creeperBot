c8e983595dfaf823aa17fa080df5963dc7dc8fb4
Deepak Singh <61933667+creepy-pirate@users.noreply.github.com>
2021-09-16 21:10:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:          '5nSqJN4Bes1KmXQSUYXvowWb6',
-    consumer_secret:      'L4lH7uITOsXL7PRME2UKOlZRBcCpZLCqaNOjxcXeE4SNTJ70dt',
-    access_token:         '1401766359780888579-LStYRMAiEddIxsanXpMulBQAECb7tf',
-    access_token_secret:  'e0T92zeOuaoGnPRMbdmqjCgnb1EC7Vdpuuj8GXirH7KmA'
+    consumer_key:          '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
   }
